2d69d70f0e8340bd2ae47f9e96d8a336bab4c77d
Ashish <ashishvista@gmail.com>
2025-12-02 16:51:01 +0530

    sada

--- db2.py
+++ db2.py
@@ -1,1 +1,17 @@
 ssh-keygen -t ed25519 -C "org1_key" -f ~/.ssh/id_ed25519_org1
+
+
+Example ~/.ssh/config (Final Version)
+Host *
+    AddKeysToAgent yes
+    UseKeychain yes
+
+Host gitlab-org1
+    HostName gitlab.com
+    User git
+    IdentityFile ~/.ssh/id_ed25519_org1
+
+Host gitlab-org2
+    HostName gitlab.com
+    User git
+    IdentityFile ~/.ssh/id_ed25519_org2
