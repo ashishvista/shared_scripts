6aeb1fc110edf5beecaf1ff8a47a9e7880649558
Ashish <ashishvista@gmail.com>
2025-12-10 16:03:17 +0530

    change

--- db2.py
+++ db2.py
@@ -2,3 +2,8 @@ ALTER TABLE orders
 ALTER COLUMN order_time SET DATA TYPE TIMESTAMP WITH TIME ZONE;
 
 dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+CAST(
+        TO_TIMESTAMP_TZ(REPLACE(?, 'Z', '+00:00'))
+        AS TIMESTAMP
+    )
